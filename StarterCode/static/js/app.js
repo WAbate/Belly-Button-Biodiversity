65ce7872a233961c5c201958ac85b8111007edc4
William Abate <williamabate6@gmail.com>
2022-07-13 12:29:51 -0500

    Working Commit 2
    Updating app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -17,3 +17,55 @@ function buildMetaData(sample) {
       });
     });
 }
+
+function buildCharts(sample) {
+    d3.json("samples.json").then((data) => {
+      var sampleData = data.samples;
+      var buildArray = sampleData.filter(sampleObj => sampleObj.id == sample);
+      var result = buildArray[0];
+  
+      var otu_ids = result.otu_ids;
+      var otu_labels = result.otu_labels;
+      var sample_values = result.sample_values;
+     
+      // Create a chart
+    var bubbleChart = {
+        title: "Bacteria Cultures Per Sample",
+        hovermode: "closest",
+        xaxis: { title: "OTU ID" },
+      };
+      var bubbleData = [
+        {
+          x: otu_ids,
+          y: sample_values,
+          text: otu_labels,
+          mode: "markers",
+          marker: {
+            size: sample_values,
+            color: otu_ids,
+            colorscale: "Earth"
+          }
+        }
+
+      ];// Use Plotly with bubble chart to create horizontal bar chart
+      Plotly.newPlot("bubble", bubbleData, bubbleChart);
+      
+      var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+      var barChartData = [
+        {
+          y: yticks,
+          x: sample_values.slice(0, 10).reverse(),
+          text: otu_labels.slice(0, 10).reverse(),
+          type: "bar",
+          orientation: "h",
+        }
+      ];
+
+      var barChartLayout = {
+        title: "Top 10 Bacteria Cultures Found",
+        margin: { t: 30, l: 150 }
+      };
+  
+      Plotly.newPlot("bar", barChartData, barChartLayout);
+    });
+  };
